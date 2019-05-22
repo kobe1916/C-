2a607b9bc7e59c2690706846166b5fe832ec5ed0
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-05-22 16:49:55 +0800

    Update C语言实现strlen函数.c

--- C语言实现strlen函数.c
+++ C语言实现strlen函数.c
@@ -10,3 +10,27 @@ int My_strlen(char* x)
 	return count;
 
 }
+
+//my_strlen("abc")
+//1+my_strlen("bc")
+//1+1+my_strlen("c")
+//1+1+1+my_strlen("")
+//1+1+1+0
+//3
+int My_strlen(char* x)
+{
+	if (*x == '\0')
+		return 0;
+	else
+		return 1 + My_strlen(x + 1);
+}
+
+int main()
+{
+	//char x = "I am a student !";
+	int ret = My_strlen("I am a student !");
+	printf("%d \n", ret);
+	system("pause");
+	return 0;
+}
+
