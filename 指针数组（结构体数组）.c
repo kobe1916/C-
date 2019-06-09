c5f182b0b260627b609911c9087aea9f33171d02
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-06-09 19:29:59 +0800

    Update 指针数组（结构体数组）.c

--- 指针数组（结构体数组）.c
+++ 指针数组（结构体数组）.c
@@ -1,1 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct student
+{
+	int num;
+	struct student *next;
+}stu;
+
+int * sort(int *nums[5], int n)
+{
+	int i, j, t;
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j < n; j++)
+		{
+			if (**(nums + i) > **(nums + j))
+			{
+				t = **(nums + j);
+				**(nums + j) = **(nums + i);
+				**(nums + i) = t;
+			}
+		}
+	}
+	return *nums;
+}
+
+void main()
+{
+	int i;
+	int * nums[5];
+	stu h, *p, *q;
+	scanf("%d", &h.num);
+	nums[0] = &h.num;
+	p = &h;
+	h.next = NULL;
+	for (i = 1; i < 5; i++)
+	{
+		q = (stu*)malloc(sizeof(q));
+		scanf("%d", &q->num);
+		nums[i] = &q->num;
+		p->next = q;
+		p = q;
+	}
+	sort(nums, 5);
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", **(nums + i));
+	}
+	printf("\n");
+	system("pause");
+}
